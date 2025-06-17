b1a25db235e27e7fc50ee90e7211814bd249f982
Tammam Faris <107762803+Tammam20@users.noreply.github.com>
2025-06-17 12:42:16 +0300

    Update services.nix

--- System/Modules/services.nix
+++ System/Modules/services.nix
@@ -45,6 +45,7 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ canon-capt ]
 
   # enable xone
   hardware.xone.enable = true;
