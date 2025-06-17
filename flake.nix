3c241f33c27f0d6f27272d463b7f7466e662d5fa
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-06-17 16:58:17 +0300

    hello

--- flake.nix
+++ flake.nix
@@ -2,14 +2,19 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Optional but recommended to limit the size of your system closure.
+   # inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { nixpkgs, ... } @ inputs:
+  {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+      ];
+   };
   };
 }
