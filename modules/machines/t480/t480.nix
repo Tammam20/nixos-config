338148bacdff8d396e61d55a57dd07322b097677
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-06-18 11:59:21 +0300

    update

--- modules/machines/t480/t480.nix
+++ modules/machines/t480/t480.nix
@@ -4,7 +4,6 @@
   imports = [lanzaboote.nixosModules.lanzaboote 
   nixos-06cb-009a-fingerprint-sensor.nixosModules."06cb-009a-fingerprint-sensor"
   ];
-	 ({ pkgs, lib, ... }: {
 
             environment.systemPackages = [
               # For debugging and troubleshooting Secure Boot.
@@ -25,8 +24,7 @@
 	    services."06cb-009a-fingerprint-sensor" = {                                 
   	    enable = true;                                                            
   	    backend = "python-validity";                                              
-	};   
-          })   
+	};
       ];
     };
     
