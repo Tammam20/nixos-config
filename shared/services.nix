b10132a1d8c0fa2a84f76341bda2be558a353301
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-06-28 17:50:55 +0300

    update

--- shared/services.nix
+++ shared/services.nix
@@ -36,7 +36,7 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  services.printing.drivers = with pkgs; [ canon-capt ];
+  services.printing.drivers = with pkgs; [ canon-cups-ufr2 gutenprintBin gutenprint ];
 
   # enable xone
   hardware.xone.enable = true;
