70ff9b8c8c558fcf062e7c5357ec6d10e548a005
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-12-11 14:55:34 +0300

    update

--- shared/services.nix
+++ shared/services.nix
@@ -27,9 +27,9 @@
   services.xserver.enable = true;
 
   # Change systemd timeout
-  systemd.settings.Manager = ''
-  DefaultTimeoutStopSec=10s
-  '';
+#  systemd.settings.Manager = ''
+ # DefaultTimeoutStopSec=10s
+#  '';
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
