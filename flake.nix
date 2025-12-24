98c5b9d6ea6713de0c1f9ce19ba617aa94b69419
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-12-24 21:18:16 +0300

    update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # NixOS official package source, using the nixos-25.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v1.0.0";      
