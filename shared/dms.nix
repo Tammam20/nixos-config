ca2c8d3ca0e1ec512185ee2c7fcfec64d0f42439
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-12-24 15:17:34 +0300

    update

--- shared/dms.nix
+++ shared/dms.nix
@@ -1,7 +1,26 @@
 { config, pkgs, lib, inputs, ... }:
 {
+programs.dankMaterialShell = {
+  enable = true;
 
-programs.dankMaterialShell.enable = true;
+  systemd = {
+    enable = true;             # Systemd service for auto-start
+    restartIfChanged = true;   # Auto-restart dms.service when dankMaterialShell changes
+  };
+  
+  # Core features
+  enableSystemMonitoring = true;     # System monitoring widgets (dgop)
+  enableClipboard = true;            # Clipboard history manager
+  enableVPN = true;                  # VPN management widget
+  enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
+  enableAudioWavelength = true;      # Audio visualizer (cava)
+  enableCalendarEvents = true;       # Calendar integration (khal)
+
+niri = {
+    enableKeybinds = true;   # Sets static preset keybinds
+    enableSpawn = true;      # Auto-start DMS with niri and cliphist, if enabled
+  };
+};
 programs.niri.enable = true;
 programs.dankMaterialShell.greeter = {
   enable = true;
