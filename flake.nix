0b0654aa31008485cfc2d0f7b322771723841fb0
Tammam20 <whymewhyalwaysme@protonmail.com>
2025-12-24 21:36:56 +0300

    update

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     };*/
   
     dms = {
-      url = "github:AvengeMedia/DankMaterialShell/stable";
+      url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
