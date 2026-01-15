17bd69d6143609e864edfadb6a7aab0147fdb883
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-01-15 18:08:01 +0300

    update

--- config/virtualisation.nix
+++ config/virtualisation.nix
@@ -4,7 +4,6 @@
   virtualisation.libvirtd.enable = true;
   virtualisation.podman.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
-  virtualisation.tpm.enable = true;
   virtualisation.libvirtd.qemu.swtpm.enable = true;
   virtualisation.waydroid = {
     enable = true;
