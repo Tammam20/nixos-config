3d650a4d446f7558ff796a2cdad6f340a8721f56
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-01-27 14:15:06 +0300

    update

--- devices/t480/t480.nix
+++ devices/t480/t480.nix
@@ -75,10 +75,10 @@ imports = [
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
         
-        RUNTIME_PM_ON_AC = "auto";
+        RUNTIME_PM_ON_AC = "on";
         RUNTIME_PM_ON_BAT = "auto";
 	
-	USB_AUTOSUSPEND = 0;
+	#USB_AUTOSUSPEND = 0;
 	
 	#USB_DENYLIST = "06cb:009a";
 
