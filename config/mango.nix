13f4369f82c000e8c340b30c0029bd6e7071b1cb
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-02-04 18:29:57 +0300

    update

--- config/mango.nix
+++ config/mango.nix
@@ -1,50 +1,49 @@
 { pkgs, ... }:
 
 {
-  /*imports = [
-    inputs.mango.nixosModules.mango
-  ];*/
-
   services.displayManager.ly.enable = true;
   security.soteria.enable = true;
   programs.dconf.enable = true;
   programs.mango.enable = true;
-  #programs.mango.package = ;
+  programs.mango.package = (pkgs.mango.mango.symlinkJoin {
+	    name = "mango";
+	    buildInputs = [ pkgs.mango.makeWrapper ];
+	    paths = [ pkgs.mango.mango ];
+	    postBuild = ''
+	        wrapProgram $out/bin/mango
+	          --append-flags "-c ${../progconfig/mango/config.conf}" 
+	        '';
+      });
 
-  environment.systemPackages = [
-    pkgs.foot
-    pkgs.wmenu
-    pkgs.wl-clipboard
-    pkgs.grim
-    pkgs.slurp
-    pkgs.swaybg
-    pkgs.networkmanagerapplet
-    pkgs.blueman
-    pkgs.sway-audio-idle-inhibit
-    pkgs.swayidle
-    pkgs.swayosd
-    pkgs.swaynotificationcenter
-    pkgs.wlsunset
-    pkgs.papirus-icon-theme
-    pkgs.brightnessctl
-    pkgs.wlogout
-    (pkgs.symlinkJoin {
-      name = "mango";
-      buildInputs = [ pkgs.makeWrapper ];
-      paths = [ pkgs.mango ];
-      postBuild = ''
-        wrapProgram $out/bin/mango --append-flags "-c ${../progconfig/mango/config.conf}"
-      '';
-    })
+  environment.systemPackages = with pkgs; [
+    foot
+    wmenu
+    wl-clipboard
+    grim
+    slurp
+    swaybg
+    networkmanagerapplet
+    blueman
+    sway-audio-idle-inhibit
+    swayidle
+    swayosd
+    swaynotificationcenter
+    wlsunset
+    papirus-icon-theme
+    brightnessctl
+    wlogout
 
-    (pkgs.symlinkJoin {
-      name = "waybar";
-      buildInputs = [ pkgs.makeWrapper ];
-      paths = [ pkgs.waybar ];
-      postBuild = ''
-        wrapProgram $out/bin/waybar --append-flags "-c ${../progconfig/waybar/config.jsonc} -s ${../progconfig/waybar/style.css}"
-      '';
-    })
+    
+
+      (pkgs.symlinkJoin {
+	    name = "waybar";
+	    buildInputs = [ pkgs.makewrapper ];
+	    paths = [ pkgs.waybar ];
+	    postBuild = ''
+	        wrapProgram $out/bin/waybar
+	        --append-flags "-c ${../progconfig/waybar/config.jsonc} -s ${../progconfig/waybar/style.css}" 
+	        '';
+      })
   ];
 
   fonts.packages = with pkgs; [
