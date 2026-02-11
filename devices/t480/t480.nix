2c60e9c25ceef3312dcf2a3f02beb7105a4f9573
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-02-11 16:19:27 +0300

    update

--- devices/t480/t480.nix
+++ devices/t480/t480.nix
@@ -84,7 +84,7 @@ imports = [
         CPU_DRIVER_OPMODE_ON_AC = "active";
         CPU_DRIVER_OPMODE_ON_BAT = "active";
         
-        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
         CPU_BOOST_ON_AC = 1;
