2d13ce47f416fff3014bbdf9e5726e487d128645
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-02-11 17:45:10 +0300

    update

--- config/cloudflare.nix
+++ config/cloudflare.nix
@@ -5,5 +5,5 @@
   environment.systemPackages = [ pkgs.cloudflare-warp ]; # for warp-svc
   systemd.packages = [ pkgs.cloudflare-warp ]; # for warp-cli
   systemd.targets.multi-user.wants = [ "warp-svc.service" ]; # causes warp-svc to be started automatically
-  systemd.user.services.warp-taskbar.wantedBy = [ "graphical.target" ];
+#  systemd.user.services.warp-taskbar.wantedBy = [ "graphical.target" ];
 }
