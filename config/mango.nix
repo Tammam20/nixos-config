dc4dfabbf2e91b0afd774cd4a87a8638a34d2ac8
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-02-11 21:26:22 +0300

    update

--- config/mango.nix
+++ config/mango.nix
@@ -3,8 +3,17 @@
 {
   services.xserver.enable = true;
   #services.displayManager.sddm.wayland.compositor = "";
-  services.xserver.displayManager.lightdm.enable = false;
+#  services.xserver.displayManager.lightdm.enable = false;
   services.gvfs.enable = true;
+  programs.uwsm.enable = true;
+  programs.uwsm.waylandCompositors = {
+  mango = {
+  prettyName = "mango";
+  comment = "mango compositor managed by UWSM";
+  binPath = "/run/current-system/sw/bin/mango";
+};
+
+};
   security.soteria.enable = true;
 #  security.polkit.package = pkgs.kdePackages.polkit-qt-1;
   programs.dconf.enable = true;
