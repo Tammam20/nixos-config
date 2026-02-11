e8e84007f6ac525fe8fb78d1049185fb1b0c755b
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-02-11 22:27:17 +0300

    update

--- config/systemd.nix
+++ config/systemd.nix
@@ -3,7 +3,7 @@
   # Change systemd timeout
   systemd.settings.Manager = {
   DefaultTimeoutStopSec = "10s";
-  DefaultRestartSec = "1s":
+  DefaultRestartSec = "1s";
   DefaultStartLimitIntervalSec = "0";
 };
 systemd.network.wait-online.enable = false;
