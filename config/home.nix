9ec43bb5ae74c1c8f6e44f866eb6a9700dd03cc3
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-02-12 13:37:29 +0300

    update

--- config/home.nix
+++ config/home.nix
@@ -1,9 +1,9 @@
-{ pkgs, config, ... }:
+{ ... }:
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  username = "tammam";
-  homeDirectory = "/home/tammam";
+  home.username = "tammam";
+  home.homeDirectory = "/home/tammam";
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -13,7 +13,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  stateVersion = "25.11";
+  home.stateVersion = "25.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
