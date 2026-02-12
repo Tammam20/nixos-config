1656e1cf9f3fe8f3523da40d4dfae3f557929973
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-02-12 14:01:42 +0300

    update

--- config/pipewire.nix
+++ config/pipewire.nix
@@ -3,6 +3,7 @@
   # pipewire
   services.pipewire.enable = true;
   services.pulseaudio.enable = false;
+  services.pipewire.jack.enable = true;
   services.pipewire.pulse.enable = true; # Enable PulseAudio server emulation.
   services.pipewire.wireplumber.enable = true; # Enable Wireplumber, a modular session / policy manager for PipeWire
   services.pipewire.alsa.enable = true; # Enable ALSA support.
