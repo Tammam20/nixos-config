3378b5c3764d977bd389b45da0e31bc89e924a3a
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-02-13 17:46:05 +0300

    update

--- flake.nix
+++ flake.nix
@@ -41,6 +41,7 @@
 	      nixos-06cb-009a-fingerprint-sensor.nixosModules."06cb-009a-fingerprint-sensor"
 	      lanzaboote.nixosModules.lanzaboote 
 	      mango.nixosModules.mango
+        mango.hmModules.mango
 	      determinate.nixosModules.default
         home-manager.nixosModules.home-manager
         #impermanence.nixosModules.impermanence
